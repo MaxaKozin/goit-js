48c87346cb2ac4010c400718877a561a1e1c770f
Maxim Kozin <maxa.kozin@gmail.com>
2020-04-16 16:50:18 +0300

    some corrections to settings and to task-6

--- goit-js-hw-01/task-6/js/task-6.js
+++ goit-js-hw-01/task-6/js/task-6.js
@@ -1,9 +1,9 @@
 let total = 0;
 let input;
 
-while (input !== null) {
-  input = prompt('Введите число:');
+do {
+  input = prompt('Введите число:', '');
   total += Number(input);
-}
+} while (input !== null);
 
 alert(`Общая сумма чисел равна : ${total}`);
